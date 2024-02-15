67c05c9fa58dc8b214be98905d853a3153f11cd0
Calvin Oker <calvinoker8@gmail.com>
2024-02-15 12:03:53 -0800

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navigation from "./components/Navigation";
+import Home from "./components/Home";
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import Students from "./components/Students";
+import Manage from "./components/Manage";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navigation />
+      <Routes>
+         <Route exact path="/" element={<Home/>} />
+         <Route path="/students" element={<Students/>} />
+         <Route path="/manage" element={<Manage/>} />
+       </Routes>
+    </BrowserRouter>
+  );
+};
+export default App;
